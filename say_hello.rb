a6b246079f155585d96ae49f711dc56250ae3d3d
Darrien Huntley <dhuntleypro@gmail.com>
2020-03-21 19:00:45 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,8 @@
 # Build your say_hello method here
 
 
-def say_hello(name="Ruby programmer")
+def say_hello(name = "Ruby programmer")
   puts "Hello #{name}!"
 end
 
 say_hello()
-
-say_hello("darrien")
